1c92d37ab0791a18fa1f1f424b5c15be5be7c195
Ricardo Kelvin <rickelvin97@outlook.com>
2024-02-07 23:52:56 -0300

    .

--- src/controllers/tag_creator_controller_test.py
+++ src/controllers/tag_creator_controller_test.py
@@ -1,14 +1,14 @@
+from unittest.mock import patch
+from src.drivers.barcode_handler import BarcodeHandler
 from .tag_creator_controller import TagCreatorController
 
-
-def test_create():
+@patch.object(BarcodeHandler, 'create_barcode')
+def test_create(mock_create_barcode):
     mock_value = "image_path"
+    mock_create_barcode.return_value = mock_value
     tag_creator_controller = TagCreatorController()
 
     result = tag_creator_controller.create(mock_value)
-
-    print()
-    print(result)
 
     assert isinstance(result, dict)
     assert "data" in result
