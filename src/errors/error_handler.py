488f5addbc00c9fca8c7e3d94f90337bbd27497a
Ricardo Kelvin <rickelvin97@outlook.com>
2024-02-07 22:59:06 -0300

    fix: add validation method and error handling

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,12 +1,25 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
-    return HttpResponse(
-        status_code=500,
-        body={
-            "errors": [{
-                "title": "Server Error",
-                "detail": str(error)
-            }]
-        }
-    )
+    if isinstance(error, HttpUnprocessableEntityError):
+        #  enviar para um log
+        # enviar um emial de notificação
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "details": error.message
+                }]
+            }
+        )
+    # return HttpResponse(
+    #     status_code=500,
+    #     body={
+    #         "errors": [{
+    #             "title": "Server Error",
+    #             "detail": str(error)
+    #         }]
+    #     }
+    # )
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 from src.errors.error_handler import handle_errors
-
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
@@ -10,10 +10,10 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 def creat_tags():
     response = None
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
-        tag_creator_view.validate_and_create(http_request)
         response = tag_creator_view.validate_and_create(http_request)
     except Exception as exception:
         response = handle_errors(exception)
